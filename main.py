cc11de8f670e4e87eef510308f30cf14d32ac09a
tanvin296 <tanvinkalra@gmail.com>
2024-07-24 12:42:48 +0530

    Add logic to fetch letters using selenium

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
 from Trie import Trie
 
 def readWordsFromFile(file_path):
@@ -15,13 +19,31 @@ def readWordsFromFile(file_path):
 
 if __name__ == "__main__":
 
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver.maximize_window()
+    driver.get("https://spellbee.org/")
+
+    hexLinks = driver.find_elements(By.CSS_SELECTOR, 'a.hexLink')
+    letters = []
+    centerLetter = None
+
+
+    # Loop through each <a> tag and find <p> tags within it
+    for aTag in hexLinks:
+        aId = aTag.get_attribute('id')
+            
+        p_tags = aTag.find_elements(By.TAG_NAME, 'p')
+        for p in p_tags:
+            letter = p.text.lower() 
+            if aId == "center-letter":
+                centerLetter = letter
+            letters.append(letter)
+    
+
     trie = Trie()
     words = readWordsFromFile('enable1.txt')
     for word in words:
         trie.insert(word)
-    
-    letters = "arwihos"
-    centerLetter = "s"
 
     valid_words = trie.formWords(letters, centerLetter)
     print(valid_words)
